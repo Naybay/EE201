dc9f6479a908556d5cb691447b746dcd29fb260d
Naybay <nathanholterhoff@gmail.com>
2023-02-09 13:55:05 -0800

    Updated to include schematics

---
+++ import serial.py
@@ -0,0 +1,11 @@
+import serial
+import csv
+
+arduino_port = "COM6" #serial port of Arduino
+baud = 9600 #arduino uno runs at 9600 baud
+fileName="analog-data.csv" #name of the CSV file generated
+
+ser = serial.Serial(arduino_port, baud)
+print("Connected to Arduino port:" + arduino_port)
+file = open(fileName, "a")
+print("Created file")
